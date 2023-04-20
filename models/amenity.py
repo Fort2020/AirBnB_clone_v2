7e5200feb096bf4a7f0fe24ba3d3b2bde457e1b3
fort2020 <frankfortdata@gmail.com>
2023-04-20 09:51:55 -0700

    OK

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
-from models.base_model import Base
 from sqlalchemy.orm import relationship
 
 
--- models/place.py
+++ models/place.py
@@ -2,8 +2,8 @@
 """ Place Module for HBNB project """
 import models
 from os import getenv
-from models.base_model import BaseModel
 from models.base_model import Base
+from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.review import Review
 from sqlalchemy import Column
@@ -13,6 +13,15 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import Table
 from sqlalchemy.orm import relationship
+
+
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
