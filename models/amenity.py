b67452b67caae2f9cb78e5dce11fb65f3acb720e
fort2020 <frankfortdata@gmail.com>
2023-04-22 14:18:21 -0700

    Fixing Task 10.

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column
-from sqlalchemy import String
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+import sqlalchmey import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    """Represents an Amenity for a MySQL database"""
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
