db9ead15156afd9583d3682bc41c13073cc2d508
fort2020 <frankfortdata@gmail.com>
2023-05-20 12:51:16 -0700

    Snail Internet Connection Cant Kill Man

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application with two routings on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+This is 0x04. AirBnB Clone - Web Framework README.md
